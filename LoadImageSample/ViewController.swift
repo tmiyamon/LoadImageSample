242c5ed73f7169328a2ccf902725be6a79b407de
tmiyamon <miyamototakuya@gmail.com>
2017-01-29 02:34:12 +0900

    add loading image sample

--- LoadImageSample/ViewController.swift
+++ LoadImageSample/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://d1l0ymoeaox3e3.cloudfront.net/2015/07/24/01/38/06/201/file")!
+        
+        self.imageView.af_setImage(withURL: url)
     }
 
     override func didReceiveMemoryWarning() {
